7d38d28fc8e88a6d8f5f9345cca24f2373d1411b
Jeremiah <jsnidar@gmail.com>
2021-08-03 22:39:08 -0500

    trying to color code dropdown

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
   //prevent submit from carrying out its default action using preventDefaul()
   const submitButton = document.querySelector("#create-task-form > input[type=submit]:nth-child(3)")
   submitButton.addEventListener('click', e => e.preventDefault())
@@ -12,20 +13,43 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 // As a user, I should be able to click some form of a submit button.
 // As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
-submitButton.addEventListener('click', function (e) {
-  let li = document.createElement('li');
+let buttonNum = 0
+let liNum =  0
+
+const subBtn = submitButton.addEventListener('click', function (e) {
+  liNum++
+  let li = document.createElement(`li`);
   listLocation.appendChild(li);
-  listLocation.lastChild.innerHTML = descField.value + '<input type="checkbox" name="taskChecker"></input>'
-  console.log(listLocation);
+  listLocation.lastChild.id = `li${liNum}`
+  buttonNum++
+  newButton = `<button type="button" id = btn${buttonNum} >x</button>`
+  newBtnId = '.#btn' + buttonNum.toString()
+  const priority = `<label for="new-task-priority"></label>
+    <select id = drp${buttonNum} name="priority">
+      <option value="high">High Priority</option>
+      <option value="med">Medium Priority</option>
+      <option value="low">Low Priority</option>
+      <input type='submit'>
+    </select>`
+  listLocation.lastChild.innerHTML = descField.value +  newButton + priority
+  
+  document.getElementByID(`drp${buttonNum}`).value.addEventListener('click', function (e) {
+    let color
+    if (e.value = 'high') {
+  color = listLocation.firstChild.style.color = 'red'
+  }else if(e.value = 'med') {
+  color = listLocation.firstChild.style.color = 'orange'
+  }else if(e.value = 'low') {
+  color = listLocation.lastChild.style.color = 'green'}
+  return color
+  })
+
+  document.getElementById(`btn${buttonNum}`).addEventListener('click', function (e) {
+    console.log(e.target)
+    e.target.parentNode.remove()
+  
+      })   
 })   
 
-listLocation.addEventListener('click', function (e) {
-  for(let li of listLocation) {
-    if (li.value === 'on') {
-      li.remove()
-    }
-  }
-})
-
-});
+}); 
 
